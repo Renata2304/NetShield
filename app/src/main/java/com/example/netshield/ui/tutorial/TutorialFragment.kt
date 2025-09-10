51f94b65a3d38e08e79e53d821ff2e1024f52ca0
Renata <94386250+Renata2304@users.noreply.github.com>
2025-09-10 21:16:30 +0300

    Logo modif + minor changes to home screen

---
+++ app/src/main/java/com/example/netshield/ui/tutorial/TutorialFragment.kt
@@ -0,0 +1,11 @@
+package com.example.netshield.ui.tutorial
+
+import androidx.fragment.app.Fragment
+import com.example.netshield.databinding.FragmentTutorialBinding
+
+class TutorialFragment : Fragment(){
+
+    private var _binding: FragmentTutorialBinding? = null
+
+    private val binding get() = _binding!!
+}
